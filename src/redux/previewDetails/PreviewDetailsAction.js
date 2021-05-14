5433bcffbb02b66ebe300cf46480142d0c1f4890
kenrazalan <kennethrazalan29@gmail.com>
2021-05-14 20:00:09 +0800

    add Preview Details loading state

--- src/redux/previewDetails/PreviewDetailsAction.js
+++ src/redux/previewDetails/PreviewDetailsAction.js
@@ -1,13 +1,37 @@
-import {FETCH_PREVIEW_DETAILS} from './PreviewDetailsType'
+import {FETCH_PREVIEW_DETAILS, FETCH_PREVIEW_DETAILS_REQUEST, FETCH_PREVIEW_DETAILS_SUCCESS} from './PreviewDetailsType'
 import axios from 'axios'
 
-export const fetchPreviewDetails = (type,id) => async dispatch =>{
+// export const fetchPreviewDetails = (type,id) => async dispatch =>{
         
-        const res = await axios(
-            `https://api.themoviedb.org/3/${type}/${id}?api_key=${process.env.REACT_APP_API_KEY}`
-        ) 
-        dispatch({
-            type: FETCH_PREVIEW_DETAILS,
-            payload: res.data
-        })
+//         const res = await axios(
+//             `https://api.themoviedb.org/3/${type}/${id}?api_key=${process.env.REACT_APP_API_KEY}`
+//         ) 
+//         dispatch({
+//             type: FETCH_PREVIEW_DETAILS,
+//             payload: res.data
+//         })
+//     }
+
+    export const fetchPreviewDetailsRequest= () => {
+        return{
+            type: FETCH_PREVIEW_DETAILS_REQUEST,
+        }
     }
+    export const fetchPreviewDetailsSuccess= (data) => {
+        return{
+            type: FETCH_PREVIEW_DETAILS_SUCCESS,
+            payload: data
+        }
+    }
+    
+    export const fetchPreviewDetails = (type,id) =>{
+        return (dispatch) =>{
+            dispatch(fetchPreviewDetailsRequest())
+            axios.get(`https://api.themoviedb.org/3/${type}/${id}?api_key=${process.env.REACT_APP_API_KEY}`)
+            .then(res=>{
+                const users = res.data
+                dispatch(fetchPreviewDetailsSuccess(users))
+            })
+        }
+    }
+    
--- src/redux/previewDetails/PreviewDetailsReducer.js
+++ src/redux/previewDetails/PreviewDetailsReducer.js
@@ -1,13 +1,33 @@
-import {FETCH_PREVIEW_DETAILS} from './PreviewDetailsType'
+import {FETCH_PREVIEW_DETAILS_REQUEST, FETCH_PREVIEW_DETAILS_SUCCESS} from './PreviewDetailsType'
 
 const initialState = {
-    item:{}
+    item:{},
+    loading: false
 }
 
-const previewDetailsReducer =  (state=initialState,action)=>{
+// const previewDetailsReducer =  (state=initialState,action)=>{
+//     switch(action.type){
+//         case FETCH_PREVIEW_DETAILS:
+//             return {
+//                 items:action.payload
+//             }
+//         default:
+//             return state
+//     }
+// }
+// export default previewDetailsReducer
+
+
+const previewDetailsReducer = (state=initialState,action) => {
     switch(action.type){
-        case FETCH_PREVIEW_DETAILS:
+        case FETCH_PREVIEW_DETAILS_REQUEST:
             return {
+                ...state,
+                loading:true
+            }
+        case FETCH_PREVIEW_DETAILS_SUCCESS:
+            return {
+                loading: false,
                 items:action.payload
             }
         default:
