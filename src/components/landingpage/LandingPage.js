4e5cbea3205c4bb9d7b6936cdfeec74fc64780f6
kenrazalan <kennethrazalan29@gmail.com>
2021-05-14 11:43:49 +0800

    add motion landing page

--- src/components/landingpage/LandingPage.js
+++ src/components/landingpage/LandingPage.js
@@ -3,6 +3,7 @@ import Movies from '../movies//Movies'
 import Banner from './Banner'
 import {useSelector,useDispatch} from 'react-redux'
 import {fetchUpcomings,fetchTopRated} from '../../redux'
+import { motion } from 'framer-motion'
 
 function LandingPage() {
     const dispatch = useDispatch();
@@ -12,14 +13,32 @@ function LandingPage() {
     },[dispatch])
     const upcomings = useSelector(state => state.upcomings.items) 
     const topRated = useSelector(state => state.topRated.items) 
-    console.log(upcomings)
-    console.log(topRated)
+    const landingPageVariants={
+        hidden:{
+            x:'-100vw'
+        },
+        visible:{
+            x:0,
+            transition:{
+                type:'tween',
+                duration:.4
+            }
+        },
+        exit:{
+            x:'-100vw',
+        }
+    
+    }
     return (
-        <>
+        <motion.div
+        variants={landingPageVariants}
+        initial='hidden'
+        animate='visible'
+        exit={{x:'-100%'}} >
             <Banner/>
             <Movies results={upcomings} title="Upcoming Movies"/>
             <Movies results={topRated} title="Top Rated Movies"/>
-        </>
+        </motion.div>
     )
 }
 
