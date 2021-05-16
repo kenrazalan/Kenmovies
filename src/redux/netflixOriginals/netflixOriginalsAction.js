f9a82f82b12a86c75a8d600d66ed215ffa432597
kenrazalan <kennethrazalan29@gmail.com>
2021-05-16 16:12:46 +0800

    add loading state netflixoriginals

--- src/redux/netflixOriginals/netflixOriginalsAction.js
+++ src/redux/netflixOriginals/netflixOriginalsAction.js
@@ -1,13 +1,38 @@
-import {FETCH_NETFLIX_ORIGINALS} from './netflixOriginalsType'
+import {FETCH_NETFLIX_ORIGINALS_REQUEST,FETCH_NETFLIX_ORIGINALS_SUCCESS} from './netflixOriginalsType'
 import axios from 'axios'
 
 
-export const fetchNetflixOriginals = () => async dispatch =>{
-    const res = await axios(
-        `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&with_networks=213`
-    )
-    dispatch({
-        type: FETCH_NETFLIX_ORIGINALS,
-        payload: res.data
-    })
+
+// export const fetchNetflixOriginals = () => async dispatch =>{
+//     const res = await axios(
+//         `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&with_networks=213`
+//     )
+//     dispatch({
+//         type: FETCH_NETFLIX_ORIGINALS,
+//         payload: res.data
+//     })
+// }
+
+export const fetchNetflixOriginalsRequest= () => {
+    return{
+        type: FETCH_NETFLIX_ORIGINALS_REQUEST,
+    }
 }
+export const fetchNetflixOriginalsSuccess= (data) => {
+    return{
+        type: FETCH_NETFLIX_ORIGINALS_SUCCESS,
+        payload: data
+    }
+}
+
+export const fetchNetflixOriginals = () =>{
+    return (dispatch) =>{
+        dispatch(fetchNetflixOriginalsRequest())
+        axios.get(`https://api.themoviedb.org/3/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&with_networks=213`)
+        .then(res=>{
+            const users = res.data
+            dispatch(fetchNetflixOriginalsSuccess(users))
+        })
+    }
+}
+
--- src/redux/netflixOriginals/netflixOriginalsReducer.js
+++ src/redux/netflixOriginals/netflixOriginalsReducer.js
@@ -1,13 +1,32 @@
-import {FETCH_NETFLIX_ORIGINALS} from './netflixOriginalsType'
+
+import {FETCH_NETFLIX_ORIGINALS_REQUEST,FETCH_NETFLIX_ORIGINALS_SUCCESS} from './netflixOriginalsType'
 
 const initialState = {
-    items: []
+    items: [],
+    loading: false
 }
 
-const netflixOriginalsReducer =  (state=initialState,action)=>{
+// const netflixOriginalsReducer =  (state=initialState,action)=>{
+//     switch(action.type){
+//         case FETCH_NETFLIX_ORIGINALS:
+//             return {
+//                 items:action.payload
+//             }
+//         default:
+//             return state
+//     }
+// }
+
+const netflixOriginalsReducer = (state=initialState,action) => {
     switch(action.type){
-        case FETCH_NETFLIX_ORIGINALS:
+        case FETCH_NETFLIX_ORIGINALS_REQUEST:
             return {
+                ...state,
+                loading:true
+            }
+        case FETCH_NETFLIX_ORIGINALS_SUCCESS:
+            return {
+                loading: false,
                 items:action.payload
             }
         default:
