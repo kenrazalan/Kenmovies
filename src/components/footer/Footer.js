bcf6de5d62b7d3a5f5be661201f15bca9396bfa9
kenrazalan <kennethrazalan29@gmail.com>
2021-05-21 14:58:09 +0800

    add footer link

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,11 @@ function Footer() {
     return (
         <div style={{margin:"30px 0 40px 0"}}>
             <h4 style={{textAlign: 'center'}}>Made by
-            <span style={{color:"rgb(59, 179, 59)"}}> Kenneth Razalan</span></h4>
+            <span style={{color:"rgb(59, 179, 59)"}}>{" "} 
+            <a href="https://kennethrazalan.netlify.app/" rel="noreferrer" target="_blank">
+                Kenneth Razalan </a>
+            </span>
+            </h4>
         </div>
     )
 }
