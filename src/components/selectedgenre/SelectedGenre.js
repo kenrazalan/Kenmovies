930efecba3acdc5c01e9070a6a8736993392231d
kenrazalan <kennethrazalan29@gmail.com>
2021-05-21 14:36:17 +0800

    add selectedGenre pagination

--- src/components/selectedgenre/SelectedGenre.js
+++ src/components/selectedgenre/SelectedGenre.js
@@ -1,8 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router';
 import { fetchSelectedGenre } from '../../redux';
 import Movies from '../movies/Movies';
+import Pagination from '../pagination/Pagination';
 
 function SelectedGenre() {
     const dispatch = useDispatch();
@@ -13,11 +15,28 @@ function SelectedGenre() {
     },[dispatch,genresId])
     const selectedGenre = useSelector(state => state.selectedGenre.items)
     const isLoading = useSelector(state => state.selectedGenre.loading)
+    const [currentPage,setCurrentPage] = useState(selectedGenre?.page)
     const x = genreType.charAt(0).toUpperCase() + genreType.slice(1)
     console.log(selectedGenre)
+
+        const handleChange =(page) =>{
+        if (selectedGenre?.page !== page) {
+            dispatch(fetchSelectedGenre(genresId,page));
+            setCurrentPage(page)
+            window.scrollTo(0,0)
+          }
+    }
     return (
         <div style={{marginTop:"100px"}}>
             <Movies results={selectedGenre?.results} isLoading={isLoading} title={x}/>
+            <Pagination 
+                activePage={selectedGenre?.page}
+                itemsCountPerPage={1}
+                pageRangeDisplayed={10}
+                totalItemsCount={selectedGenre?.total_pages}
+                totalPage={selectedGenre?.total_pages}
+                onChange={handleChange}
+                />
         </div>
     )
 }
