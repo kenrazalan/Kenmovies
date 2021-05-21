220f3499c18bb0b9b2de610a94e15e82b5f5f49e
kenrazalan <kennethrazalan29@gmail.com>
2021-05-21 13:41:24 +0800

    add link to genres

--- src/components/genre/GenreCard.js
+++ src/components/genre/GenreCard.js
@@ -1,20 +1,22 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 
 
 function GenreCard({genreType,id}) {
-    
+    const genreLink = genreType.toLowerCase().replace(/\s+/g, '-')
     const genreName = genreType.split(' ')[0]
-    console.log(genreType)
+    console.log(genreLink)
     return (
         <div className="genre pointer" >
             {genreType && 
                 <div className="image-container">
+                    <Link to={`/genres/${genreLink}/${id}`}>
                     <div className="imahe" style={{
                         background: `url(../${genreName}.jpg) 0% 0% / cover no-repeat`
                     }}>
                         <p>{genreType}</p>
                     </div>
+                    </Link>
                         
                         {/* <img 
                         src={`../${genreType}.jpg`} 
